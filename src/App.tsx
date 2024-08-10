b21e2df6f8800b3fd7df29988302eeb579fe22d3
adityabhardwaj058 <adityabhardwaj058@gmail.com>
2024-08-10 16:23:07 +0530

    changed resolution

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export const App = () => {
         );
         const images = response.data.map((photo: Photo) => ({
           ...photo,
-          download_url: `https://picsum.photos/id/${photo.id}/100/100`,
+          download_url: `https://picsum.photos/id/${photo.id}/600/600`,
         }));
         const imageUrls = images.map((photo: Photo) => photo.download_url);
         setPhotos(imageUrls);
